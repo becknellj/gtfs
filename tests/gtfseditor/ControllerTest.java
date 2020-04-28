93ef69ff6fa023c457027a645cd2bc47123f9782
becknellj <becknell17@gmail.com>
2020-04-28 04:20:22 -0500

    Added 3 tests with feature 12 focus

--- tests/gtfseditor/ControllerTest.java
+++ tests/gtfseditor/ControllerTest.java
@@ -4,11 +4,13 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.LinkedList;
 
+import static gtfseditor.StopTime.timeShift;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ControllerTest {
@@ -17,6 +19,15 @@ class ControllerTest {
     Application GTFS;
     File f1;
     File f2;
+    File f3;
+    File testStop;
+    File testRoute;
+    File testTrip;
+    LinkedList<StopTime> testTimes;
+    StopTime a;
+    StopTime b;
+    StopTime c;
+
 
     @BeforeEach
     void setUp() {
@@ -24,6 +35,12 @@ class ControllerTest {
         c1 = new Controller();
         f1 = new File("text.txt");
         f2 = new File("../../trips.txt");
+        f3 = new File("C:\\Users\\becknellj\\Documents\\GTFS_MCTS\\stop_times.txt");
+        testTimes = new LinkedList<>();
+        a = new StopTime("21736564_2535", "08:52:00", "08:52:00", "4664", "2", "", "0", "0");
+        b = new StopTime("21736564_2535", "08:52:00", "08:52:00", "4664", "2", "", "0", "0");
+        c = new StopTime("21736564_2535", "08:52:00", "08:52:00", "4664", "2", "", "0", "0");
+
     }
 
     @AfterEach
@@ -31,15 +48,16 @@ class ControllerTest {
     }
 
 
-
     //Andrew Nebel
     //This test makes sure that if there is no file to parse
     //that it is thrown up to the importFiles method so that
     //an alert is thrown to the user.
     @Test
-    void parseFilesNullPointer(){
+    void parseFilesNullPointer() {
         assertThrows(NullPointerException.class,
-                () ->{c1.parseFiles(null,null,null,null);});
+                () -> {
+                    c1.parseFiles(null, null, null, null);
+                });
     }
 
     //Andrew Nebel
@@ -47,9 +65,11 @@ class ControllerTest {
     //that it is thrown up to the importFiles method so that
     //an alert is thrown to the user.
     @Test
-    void parseFilesFileNotFound(){
+    void parseFilesFileNotFound() {
         assertThrows(FileNotFoundException.class,
-                () ->{c1.parseFiles(f1, f1, f1, f1);});
+                () -> {
+                    c1.parseFiles(f1, f1, f1, f1);
+                });
     }
 
     //Andrew Nebel
@@ -57,13 +77,93 @@ class ControllerTest {
     //hashtable and that their info is the same as excepted based on the
     //.txt file they are in
     @Test
-    void filesToTripsEqualTrip(){
+    void filesToTripsEqualTrip() {
         try {
             BufferedReader b1 = new BufferedReader(new FileReader(f2));
             c1.fileToTrips(b1, "trips.txt");
-            assertEquals("64",Controller.GTFSeditor.trips.get("21736564_2535").getRoute_id());
-        } catch (FileNotFoundException e){
+            assertEquals("64", Controller.GTFSeditor.trips.get("21736564_2535").getRoute_id());
+        } catch (FileNotFoundException e) {
             e.getMessage();
         }
     }
+
+    /**
+     * TESTS FOR FEATURE 12
+     * Author: Jade Becknell
+     * 1) making sure that user input is reflected in data structure (when everything is valid
+     * 2) making sure that invalid input for trip_id is handles
+     * 3) making sure that invalid attribute data is handles
+     */
+    //
+
+    /**
+     *  1 of 3
+     *  This test makes sure that the user cannot successfully enter invalid attribute values
+     */
+    @Test
+    void inputEqualsAttributeData() {
+        try {
+            BufferedReader b1 = new BufferedReader(new FileReader(f3));
+            c1.fileToStopTimes(b1, "feature12Test.txt");
+
+            StopTime.timeShift("01:01:01", Controller.GTFSeditor.stopTimes.get("21736564_2535"), "arrival", "f", 0);
+            assertEquals("09:52:01", Controller.GTFSeditor.stopTimes.get("21736564_2535").get(0).getArrival_time());
+
+        } catch (IOException E) {
+            System.out.println("d");
+        }
+    }
+
+    /**
+     * 2 of 3
+     * This tests to make sure that a user can't update a stop time with invalid input
+     * checks for invalid trip id, time input, and stop sequence
+     */
+    @Test
+    void invalidAttributeInput() {
+        testTimes.add(a);
+        testTimes.add(b);
+        testTimes.add(c);
+        try {
+
+            String userInputTime = "012345";
+            String userInputStopSeq = "aaaaa";
+            String userInputTripId = "123456789";
+
+            assertThrows(DateTimeParseException.class, () -> {
+                StopTime.timeShift(userInputTime, testTimes, "a", "f", 0);
+            });
+            assertThrows(NumberFormatException.class, () -> {
+                a.setStop_sequence(userInputStopSeq);
+            });
+            assertThrows(NullPointerException.class, () -> {
+                a.setTrip_id(userInputTripId);
+            });
+
+        } catch (Exception aa) {
+            System.out.println("You should not get this");
+            aa.printStackTrace();
+
+        }
+    }
+
+    /**
+     * 3 of 3
+     * This test makes sure the user cannot successfully enter an invalid trip id
+     * to specify which stop time group they would like to edit
+     */
+    @Test
+    void testInvalidTripIdSearch() {
+        String invalidTripId = "abcdefg"; //incorrect format
+        String invalidTripId1 = "21736564_2531"; //correct format but not found in file
+
+        assertThrows(NullPointerException.class, () -> {
+            Controller.GTFSeditor.stopTimes.get(invalidTripId).size();
+        });
+        assertThrows(NullPointerException.class, () -> {
+            Controller.GTFSeditor.stopTimes.get(invalidTripId1).size();
+        });
+    }
+
+
 }
